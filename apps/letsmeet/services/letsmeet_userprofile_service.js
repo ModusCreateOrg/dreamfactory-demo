88fced5cade0715bd0cbc3e70435de924ed9c073
Simon Prickett <simon@crudworks.org>
2016-03-25 16:34:54 -0700

    Can now get current user or selection of users by ID

--- apps/letsmeet/services/letsmeet_userprofile_service.js
+++ apps/letsmeet/services/letsmeet_userprofile_service.js
@@ -1,6 +1,11 @@
+var params, users, user;
 var result = [];
 var lodash = require("lodash.min.js");
 var md5 = require('md5.js');
+
+function getGravatarByEmailAddress(emailAddress) {
+    return 'http://gravatar.com/avatar/' + md5.encode(emailAddress) + '?d=retro';
+}
 
 if (event.request.method !== 'GET') {
     throw('Only HTTP GET is allowed on this service.');
@@ -10,22 +15,34 @@ params = event.request.parameters;
 
 if (params && params.ids) {
     users = platform.api.get('system/user?ids=' + params.ids);
-} else {
-    throw('ids is a required parameter for this service!');
-}
-
-if (users.content && users.content.resource) {
-    lodash._.each(users.content.resource, function(record) {
-        result.push({
-            id: record.id,
-            name: record.name,
-            first_name: record.first_name,
-            last_name: record.last_name,
-            email: record.email,
-            phone: record.phone,
-            profile_image_url: 'http://gravatar.com/avatar/' + md5.encode(record.email) + '?d=retro'
+    if (users.content && users.content.resource) {
+        lodash._.each(users.content.resource, function(record) {
+            result.push({
+                id: record.id,
+                name: record.name,
+                first_name: record.first_name,
+                last_name: record.last_name,
+                email: record.email,
+                phone: record.phone,
+                profile_image_url: getGravatarByEmailAddress(record.email)
+            });
         });
-    });
+    }
+} else {
+    user = platform.api.get('user/profile');
+    if (user && user.content) {
+        // We can't get the ID like this... may need to get logged in user email and search system/user for it?
+        user = user.content;
+        result = {
+            id: -1,
+            name: user.name,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            phone: user.phone,
+            profile_image_url: getGravatarByEmailAddress(user.email)
+        };
+    }
 }
 
 return { "resource": result };
